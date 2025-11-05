b44a1e7ca5c1838eab40da69a03815d8356392f0
donglq-2639 <le.quang.dong@sun-asterisk.com>
2025-11-05 15:02:21 +0700

    DongLQ: BTTH 2

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -5,3 +5,20 @@ export const getElementById = (page: Page, id: string): Locator =>
 
 export const getElementAttributeName = (page: Page, name: string): Locator =>
   page.locator(`[name="${name}"]`);
+
+export const loginAndExpectError = async ({
+  page,
+  userName,
+  password,
+}: {
+  page: Page;
+  password: string;
+  userName: string;
+}): Promise<Locator> => {
+  const userNameElement = getElementById(page, 'user-name');
+  const passwordElement = getElementById(page, 'password');
+  await userNameElement.fill(userName);
+  await passwordElement.fill(password);
+  await page.getByRole('button', { name: 'Login' }).click();
+  return page.locator(`[data-test="error"]`);
+};
